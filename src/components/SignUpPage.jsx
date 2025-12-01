40687d2a08ef1af5b04bff13d74492c840b3936d
Ronaldnur <ronaldnursunarto@gmail.com>
2025-12-01 20:23:49 +0700

    fix import

--- src/components/SignUpPage.jsx
+++ src/components/SignUpPage.jsx
@@ -1,6 +1,6 @@
 // src/components/SignUpPage.jsx
 import React, { useState } from 'react';
-import Logo from '../assets/logo.png'; 
+import Logo from '../assets/Logo.png'; 
 import WaveTop from '../assets/upperwave.png'; 
 import WaveBottom from '../assets/bottomwave.png'; 
 
